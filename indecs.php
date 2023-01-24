2a19b7bdf61ba2d5066ab0abbaba8cab3c0552ec
mutuohoro <mutuohoro123@mail.com>
2023-01-24 22:18:39 +0300

    add files for skeleton of the project

---
+++ indecs.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+	<div class="formwrap">
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+	<div class="inputbox">
+	<input type="text" placeholder="Name of product" name="nameofproduct">
+	</div>
+	<div class="inputbox">
+	<select>
+		<option value="siuu">
+		<option value="siuu2">
+	</select>
+	</div>
+	</form>
+	</div>    
+</body>
+</html>
