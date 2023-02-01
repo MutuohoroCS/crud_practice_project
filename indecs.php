2df57d4dc4414be11bf986238e7a39affdd91c55
mutuohoro <mutuohoro123@mail.com>
2023-02-01 04:47:14 +0300

    add css styling to form

--- indecs.php
+++ indecs.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+	<link rel="stylesheet" href="./inc/style.css">
 </head>
 <body>
+	<div class="formwrap1">
 	<div class="formwrap">
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 	<div class="inputbox">
@@ -14,11 +16,19 @@
 	</div>
 	<div class="inputbox">
 	<select>
-		<option value="siuu">
-		<option value="siuu2">
+		<option value="100ml">100Mls</option>
+		<option value="250ml">250Mls</option>
+		<option value="500ml">500Mls</option>
+		<option value="1l">1l</option>
 	</select>
 	</div>
+	<div class="inputbox">
+	<input type="number" name="price" placeholder="please put price">
+	</div>
 	</form>
-	</div>    
+	<button class="btn" type="submit" name="submit">SUBMIT</button>
+	</div>   
+	</div>
+	 
 </body>
 </html>
